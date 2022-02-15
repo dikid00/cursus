4de9e122c59f99eb2bb3ffc38f88d8b748780bbf
dikid00 <97414117+dikid00@users.noreply.github.com>
2022-02-15 16:46:18 +0000

    Delete libft directory

--- libft/ft_memse.c
+++
@@ -1,5 +0,0 @@
-#include "libft.h"
-
-void	*ft_memset(void *b, int c, size_t len)
-{
-
--- libft/libft/ft_isprint.c
+++
@@ -1,9 +0,0 @@
-#include "libf.h"
-
-int ft_isprint(int c)
-{
-	if (c < ' ' || c > '~')
-		return (0);
-	else
-		return (c);
-}
