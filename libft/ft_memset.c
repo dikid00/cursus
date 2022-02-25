30bbbf2c0704d5c914c9187777f1051c3f873d23
dikid00 <97414117+dikid00@users.noreply.github.com>
2022-02-25 19:37:57 +0000

    Add files via upload

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -1,12 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabarbas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/25 11:23:12 by mabarbas          #+#    #+#             */
+/*   Updated: 2022/02/25 11:29:04 by mabarbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "libft.h"
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-    unsigned char   *s;
+	unsigned char	*s;
 
-    s = (unsigned char *)b;
-    while (n-- > 0)
-        *(s++) = c;
-    return (b);
+	s = (unsigned char *)b;
+	while (len-- > 0)
+		*(s++) = c;
+	return (b);
 }
-
